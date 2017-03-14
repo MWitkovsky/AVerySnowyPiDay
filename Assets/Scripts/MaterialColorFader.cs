2dc405363c4623b90faffb66b18217b4dd920ed9
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2017-03-14 19:06:12 -0400

    color lerps and greeting setup

---
+++ Assets/Scripts/MaterialColorFader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColorFader : MonoBehaviour {
+
+    [SerializeField] private Color color1, color2;
+    [SerializeField] private float lerpSpeed;
+
+    private SpriteRenderer rend;
+    private Color currentColor;
+    private bool toColor2;
+
+	void Start () {
+        rend = GetComponent<SpriteRenderer>();
+        currentColor = color1;
+        rend.color = currentColor;
+        toColor2 = true;
+	}
+	
+	void Update () {
+        if (toColor2)
+        {
+            currentColor = Color.Lerp(currentColor, color2, lerpSpeed * Time.deltaTime);
+            if(Mathf.Abs(currentColor.r - color2.r) < 0.05f)
+            {
+                toColor2 = false;
+            }
+        }
+        else
+        {
+            currentColor = Color.Lerp(currentColor, color1, lerpSpeed * Time.deltaTime);
+            if (Mathf.Abs(currentColor.r - color1.r) < 0.05f)
+            {
+                toColor2 = true;
+            }
+        }
+        rend.color = currentColor;
+    }
+}
---
+++ Assets/Scripts/TextMeshColorFader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMeshColorFader : MonoBehaviour {
+
+    [SerializeField] private Color color1, color2;
+    [SerializeField] private float lerpSpeed;
+
+    private TextMesh textMesh;
+    private Color currentColor;
+    private bool toColor2;
+
+	void Start () {
+        textMesh = GetComponent<TextMesh>();
+        currentColor = color1;
+        textMesh.color = currentColor;
+        toColor2 = true;
+	}
+	
+	void Update () {
+        if (toColor2)
+        {
+            currentColor = Color.Lerp(currentColor, color2, lerpSpeed * Time.deltaTime);
+            if(Mathf.Abs(currentColor.r - color2.r) < 0.05f)
+            {
+                toColor2 = false;
+            }
+        }
+        else
+        {
+            currentColor = Color.Lerp(currentColor, color1, lerpSpeed * Time.deltaTime);
+            if (Mathf.Abs(currentColor.r - color1.r) < 0.05f)
+            {
+                toColor2 = true;
+            }
+        }
+        textMesh.color = currentColor;
+    }
+}
