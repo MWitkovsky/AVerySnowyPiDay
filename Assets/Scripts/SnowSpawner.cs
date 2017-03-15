b0fbb124271404380042e125229e79f755226968
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2017-03-14 20:05:08 -0400

    added snowflake handler and snowflake spawner

--- Assets/Scripts/SnowSpawner.cs
+++ Assets/Scripts/SnowSpawner.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class SnowSpawner : MonoBehaviour {
 
-	// Use this for initialization
+    [SerializeField] private GameObject snowflake;
+    [SerializeField] private float spawnsPerSecond;
+
+    private Vector3 spawnPosition;
+    private float halfWidth;
+    private float spawnCoeff;
+    private float spawnTimer;
+
 	void Start () {
-		
+        spawnPosition = transform.position;
+        halfWidth = GetComponent<Renderer>().bounds.extents.x;
+        spawnCoeff = 1.0f / spawnsPerSecond;
+        spawnTimer = spawnCoeff;
+        
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if(spawnTimer <= 0.0f)
+        {
+            spawnPosition.x = Random.Range(transform.position.x - halfWidth, transform.position.x + halfWidth);
+            Instantiate(snowflake, spawnPosition, Quaternion.identity);
+            spawnTimer = spawnCoeff;
+        }
+        else
+        {
+            spawnTimer -= Time.deltaTime;
+        }
 	}
 }
---
+++ Assets/Scripts/SnowflakeHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowflakeHandler : MonoBehaviour {
+
+    private float vxMin = 1.0f, vxMax = 5.0f;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector3.right * Random.Range(vxMin, vxMax);
+    }
+
+	private void OnCollisionEnter2D()
+    {
+        if(Random.Range(0.0f, 100.0f) > 70.0f)
+        {
+            Destroy(GetComponent<Rigidbody2D>());
+            gameObject.layer = LayerMask.NameToLayer("IdleSnowflake");
+        }
+    }
+}
